e44c47c6896ff9be2f1a6f852c5a5dbd935ea83e
c99SRS <c99SRS>
2017-02-01 23:24:18 +0530

    image-back up

---
+++ README.md
@@ -0,0 +1,7 @@
+# Scrip
+
+This script helps to copy of one folder to another . No need to copy and paste from one foder to other .  
+The script will take care of everything . 
+You just need to run this script file on terminal.
+You can also used to take back-up of certain  folders in servrer . 
+This  will take back-up all files hich is less than 1 day , you can also customize this day .
---
+++ image_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Purpose = Backup of Important Data
+#Created on 29-01-2017
+#Author = Smruti Ranjan
+#Version 1.0
+#START
+#mkdir /home/smruti/image
+
+DIR=/home/smruti/Image              #your destination directory
+SRC=/home/smruti/NetBeansProjects/ehr-platform/healthelife-ehrsolution-master/cabolabs-ehrserver-master/upload          # your source directory , change accordingly 
+if [ ! -d $DIR ]
+then
+        echo " directory " $DIR " does not exist. creating directory ........";
+        mkdir /home/smruti/Image
+        find $SRC -maxdepth 1 -mtime +1 -type f -exec cp "{}" $DIR \;
+else
+        echo "directory " $DIR " exists. Copying files .........";
+        find $SRC -maxdepth 1 -mtime +1 -type f -exec cp "{}"  $DIR \;
+fi
+echo ""
+echo " =======>>>>>>>>>  Enjoy  scripts <<<<<<<<<<===============";
+
+echo ""
+echo "     All Files copied Successfully !!!"
+echo ""
